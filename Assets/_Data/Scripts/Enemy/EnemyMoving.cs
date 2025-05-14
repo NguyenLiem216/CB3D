52e402d5c387c9ca203013261567320c6b9f0f53
PC <nguyenliem260899@gmail.com>
2025-05-14 12:34:26 +0700

    Selection Sort , Lighting, AI Find Stress, Model From Mixamo

---
+++ Assets/_Data/Scripts/Enemy/EnemyMoving.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMoving : MonoBehaviour
+{
+
+    public GameObject target;
+    public NavMeshAgent agent;
+
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
